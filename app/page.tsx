db6db87979280e99a97b4a8d1ab28ab8acbead4c
Shadik286 <shadik286@users.noreply.github.com>
2026-07-09 17:18:34 +0600

    annotate todo param type in demo page (fix build #3)

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Page() {
 
   return (
     <ul>
-      {todos?.map((todo) => (
+      {todos?.map((todo: { id: string; name: string }) => (
         <li key={todo.id}>{todo.name}</li>
       ))}
     </ul>
